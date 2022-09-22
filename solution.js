093a88e64f5b264b907b9c651c42908d5956e9dc
Abdul Jawwad <abduljawwad.md@gmail.com>
2022-09-22 12:40:07 +0100

    added deserialize()

--- solution.js
+++ solution.js
@@ -35,3 +35,56 @@ export const objectToList = (object) => {
   }
   return result;
 };
+
+export const deserialize = (serializedObject) => {
+  let headerProperty;
+  let result = [];
+  let obj = {};
+
+  for (const [key, value] of Object.entries(serializedObject)) {
+    const indexMatch = key.match(/\d{1}(?=(_))/gi);
+    if (indexMatch == null) {
+      obj = {
+        ...obj,
+        [`${key}`]: value,
+      };
+      continue;
+    }
+    const index = Number(indexMatch[0]);
+
+    headerProperty = key.match(/\w{1,}(?=([0-9]_))/gi)[0];
+
+    const objectProperty = key.match(/(?<=([0-9]_))\w{1,}/gi)[0];
+
+    if (typeof value !== "object") {
+      if (value.toString().match(/t:\d{13}/g)) {
+        const [year, month, dateTime] = new Date(
+          Number(value.match(/\d{1,13}/gi)[0])
+        )
+          .toISOString()
+          .split("-");
+        const providedDate = dateTime.match(/^\d{2}/gi)[0];
+        const formattedDate = [providedDate, month, year].join("/");
+        result[index] = {
+          ...result[index],
+          [`${objectProperty}`]: formattedDate,
+        };
+      } else {
+        result[index] = { ...result[index], [`${objectProperty}`]: value };
+      }
+    }
+
+    if (typeof value === "object" && !Array.isArray(value) && value !== null) {
+      result[index] = {
+        ...result[index],
+        [`${objectProperty}`]: deserialize(value),
+      };
+    }
+  }
+
+  obj = {
+    ...obj,
+    [`${headerProperty}`]: result,
+  };
+  return obj;
+};
